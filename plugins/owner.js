63504e8608464647503a6f6bfd5d44589a21fe9f
windro-op <94542484+windro-op@users.noreply.github.com>
2021-11-18 07:40:24 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*MMG BOT BY WINDRO*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Ente Mothalai WindrO SeR ane 🔥❤️*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
